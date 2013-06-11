2c00896c9acfcd5d78135912a9e83472939d4d70
Matt Baker <dev@mikesierra.net>
2013-06-11 14:19:00 +0100

    added some comments

--- README.md
+++ README.md
@@ -3,18 +3,28 @@ csvTo301
 
 Introduction
 --------------
-A nice simple little perl script that parses a CSV file with two colums - The current url and the redirect url - and spits out a text file with a block of formatted 301 redirect statements.  Handy if you're working on migrating a site with a large number of urls, but the architecture prevents implementing a DB solution and the only alternative is a block of statements in the .htaccess file!
+A nice simple little perl helper script that parses a CSV file with two colums - The current url and the redirect url - and spits out a text file with a block of formatted mod_alias 301 redirect statements.  Handy if you're working on migrating a site with a large number of urls, but the architecture prevents implementing a DB solution and the only alternative is a block of statements in the .htaccess file!
 
 collect the existing relative url e.g. /aboutus.htm and the relative destination link e.g. /about
 
+**Note: ** the mod_alias module will ignore 301 redirects that have a querystring appended to the url that is being redirected e.g. *http://www.baseurl.com/news?article=12*.  For urls with this structure mod_rewrite  will have to be used to redirect i.e. links produced by this script won't be effective!
+
+**Note 2: ** v0.1 doesn't sort the output
 
 Requirements
 -----------------
 perl (obviously!)
+
 Text::CSV cpan module (type "cpan Text::CSV" into terminal window to install)
 
 Usage
 --------
- perl csvto301.pl "/path/to/csvfile.csv" "/path/for/output.txt" "http(s)://baseurl of destination"
+ perl csvto301.pl "**/path/to/csvfile.csv**" "**/path/for/output.txt**" "**http(s)://baseurl of destination**"
  
- e.g. perl csvto301.pl "data.csv" "output.txt" "http://www.website.com"
+	perl csvto301.pl "data.csv" "output.txt" "http://www.website.com"
+ 
+ Example of output to output.txt
+ ------------------------------------
+ 
+ Redirect 301 /home.htm http://baseurl/home
+ Redirect 301 /aboutus.htm http://baseurl/about
--- csvto301.pl
+++ csvto301.pl
@@ -1,60 +1,91 @@
 #!/usr/bin/perl
-    use strict;
-    use warnings;
-    use Text::CSV;
-	
-	my $validArgs = checkArgs();
-	my %redirects;
-	
-	if ($validArgs) {
-		parseCSV();
-		writeTxtFile();
-	} else {
-		print "\l\n Please supply arguements:  \l\n\l\n perl csvto301.pl \"path/to/file.csv\"  \"outputfile.txt\" \"http(s)://redirecturl\"\l\n";
-	}
-	
-	sub checkArgs {
-		if (!defined $ARGV[0] || !defined $ARGV[1] || !defined $ARGV[2]) {
-			return 0;
-		} else {
-			if ($ARGV[2] !~ /^http[s]?:\/\/[\w\/.]*/) {
-				return 0;
-				last;
-			}
-			return 1;
-		}
-	}
-	
-	sub parseCSV {
-		my $file = $ARGV[0];
-		my $csv = Text::CSV->new();
-		my $count = 0;
-		my @columns;
 
-		open (CSV, "<", $file) or die " cannot open file \"$file\": $!";
-		while (<CSV>) {
-			if ($csv->parse($_)) {
-				 @columns = $csv->fields();
-				 $redirects{$columns[0] } = $columns[1];
-				 $count++;
-			} else {
-				my $err = $csv->error_input;
-					print "Failed to parse line: $err";
-			}
+## v0.1
+## 11/06/2013 dev@mikesierra.net
+
+##
+#	csvto301rewrite.pl parses 2 column csv and generates
+#	txt file with formatted apache mod_rewrite 301 redirects
+#	that can be pasted into .htaccess file
+##
+use strict;
+use warnings;
+use Text::CSV;
+
+## globally accessible variables
+my $validArgs = checkArgs(); #boolean
+my %redirects; #hash containing redirects as $redirects{oldurl} = newurl
+
+##
+#	Code - constructor if you will!
+##
+if ($validArgs) {
+	parseCSV();
+	writeTxtFile();
+} else {
+	print "\l\n Please supply arguements:  \l\n\l\n perl csvto301.pl \"path/to/file.csv\"  \"outputfile.txt\" \"http(s)://redirecturl\"\l\n";
+}
+## End of code
+
+##
+#	checkArgs subroutine checks that the script has been
+#	called with the correct number of aguements, and that 
+#	the last one at least appears to be a url
+#
+#	returns: boolean
+##
+sub checkArgs {
+	if (!defined $ARGV[0] || !defined $ARGV[1] || !defined $ARGV[2]) {
+		return 0;
+	} else {
+		if ($ARGV[2] !~ /^http[s]?:\/\/[\w\/.]*/) {
+			return 0;
+			last;
 		}
-		close CSV;
-		print "parsed $count lines in \"$file\"\l\n";
+		return 1;
 	}
+}
+
+##
+#	parseCSV subroutine attempts to open specified csv file,
+#	parse it spit values into @columns array
+#	sets key=>value pair in  global %redirects hash
+##
+sub parseCSV {
+	my $file = $ARGV[0];
+	my $csv = Text::CSV->new();
+	my $count = 0;
+	my @columns;
+
+	open (CSV, "<", $file) or die " cannot open file \"$file\": $!";
+	while (<CSV>) { # for each line of csv
+		if ($csv->parse($_)) { #call the parse function of Text::CSV
+			 @columns = $csv->fields(); #add values to array
+			 $redirects{$columns[0] } = $columns[1]; #add array values to global hash
+			 $count++;
+		} else {
+			my $err = $csv->error_input;
+				print "Failed to parse line: $err";
+		}
+	}
+	close CSV;
+	print "parsed $count lines in \"$file\"\l\n";
+}
+
+##
+#	writeTxtFile subroutine creates a text file, loops through the
+#	global %redirects hash and appends apache
+#	mod_alias 301 redirect statements to it.  
+## 
+sub writeTxtFile {
+	my $file = $ARGV[1];
+	my $baseUrl = $ARGV[2];
 	
-	sub writeTxtFile {
-		my $file = $ARGV[1];
-		my $baseUrl = $ARGV[2];
-		
-		open (FILE, ">>", $file) or die " cannot open file \"$file\": $!";
-		while (my ($key,$value) = each(%redirects)) {
-			print FILE "Redirect 301 $key $baseUrl$value\l\n";
-		}
-		close(FILE);
-		print "Redirect statements written to \"$file\"";
+	open (FILE, ">>", $file) or die " cannot open file \"$file\": $!";
+	while (my ($key,$value) = each(%redirects)) {
+		print FILE "Redirect 301 $key $baseUrl$value\l\n";
 	}
+	close(FILE);
+	print "Redirect statements written to \"$file\"";
+}
     
